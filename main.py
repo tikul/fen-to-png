1fada466dda057c561deb660a7e01583df0989d3
tikul <tikul@users.noreply.github.com>
2017-09-11 18:29:59 -0400

    Add image assets

--- main.py
+++ main.py
@@ -2,6 +2,40 @@ from PIL import Image
 
 PIECES = set("RNBKQPrnbkqp")
 FEN_PIECES = { i : ("b" if i.islower() else "w") + i.lower() for i in PIECES}
+BOARD_DIMENSIONS = 320
+BOARD_SIZE = (BOARD_DIMENSIONS, BOARD_DIMENSIONS)
+SQUARE_SIZE = 40
+PIECE_SIZE = (SQUARE_SIZE, SQUARE_SIZE)
+RESOURCES = "resources/"
+
+class Board():
+    def __init__(self, board, no=0):
+        self.result = Image.open(RESOURCES+"board.png").resize(BOARD_SIZE)
+        self.board = board
+        self.no = no
+    
+    def open_image(self, piece):
+        try:
+            im = Image.open(RESOURCES+"{}.png".format(piece))
+            return im.resize(PIECE_SIZE)
+        except:
+            print(piece+".png", "does not exist.")
+            return None
+
+    def insert(self, piece, square): #square is tuple (r,c)
+        R = square[0]*SQUARE_SIZE
+        C = square[1]*SQUARE_SIZE
+        self.result.paste(piece, (R,C), piece)
+
+    def create(self): #Fix orientation of board
+        for i in range(8):
+            for j in range(8):
+                if self.board[i][j]:
+                    piece = self.open_image(self.board[i][j])
+                    self.insert(piece, (i,j))
+
+    def to_image(self):
+        self.result.save(RESOURCES+"{}.png".format(self.no))
 
 def isInt(value):
     try:
@@ -70,7 +104,7 @@ def isValidFEN(fen):
     # if isInt(fullmove):
     #     print("Good fullmove.")
 
-def FENtoBoard(fen):
+def FENtoBoard(fen): #Fix orientation of board
     board = [["" for j in range(8)] for i in range(8)]
     board_str = fen.split()[0].split("/")
     for i, rank in enumerate(board_str):
@@ -88,10 +122,13 @@ def getFEN():
     if isValidFEN(fen):
         print("Valid FEN.")
         result = FENtoBoard(fen)
-        for row in result:
-            print(*row)
+        bb = Board(result)
+        bb.create()
+        bb.to_image()
     else:
         print("Invalid FEN.")
 
 # print(FEN_PIECES)
 getFEN()
+
+
